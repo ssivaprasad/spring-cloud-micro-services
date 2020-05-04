68c4dd3a5c8c9548e663e31d519b2bcda7a98fd2
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-04 20:22:44 +0530

    Add Spring Boot Main class and application.properties : movie-rating-service

---
+++ movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/moviemanagement/movierating/MovieRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssp.apps.microservices.moviemanagement.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRatingServiceApplication.class, args);
+    }
+
+}
