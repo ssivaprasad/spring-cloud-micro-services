c89c134ccbd1f1168b27726d5dd1047e50ee79e3
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-05 12:26:44 +0530

    Fetch configuration details for movie-info-service from cloud-config-server

--- movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/controller/MovieInfoController.java
+++ movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/controller/MovieInfoController.java
@@ -1,5 +1,6 @@
 package com.ssp.apps.microservices.movieinfo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/movie-info")
 public class MovieInfoController {
 
+    @Value("${app_name}")
+    private String appName;
+
     @GetMapping
     public String checkStatus() {
-        return "Working...!!!";
+        return "Working...!!!, APP_NAME : " + appName;
     }
 }
