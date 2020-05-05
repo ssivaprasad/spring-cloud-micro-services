ad12db76ae26b2e73deaf410ab99278cc89ee279
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-05 12:15:47 +0530

    Revert: Entity, Repository and MySQL DB properties to make application Simple. Will add them later on the need  basis

--- movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssp.apps.microservices.movieinfo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.ssp.apps.microservices.movieinfo.entity.Movie;
-
-public interface MovieRepository extends CrudRepository<Movie, String> {
-
-}
