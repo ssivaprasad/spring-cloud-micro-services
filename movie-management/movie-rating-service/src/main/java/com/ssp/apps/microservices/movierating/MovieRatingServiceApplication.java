a909f4b3b9b29dca26d96990d0c24b404462da5b
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-05 12:39:02 +0530

    Fetch configuration details for movie-rating-service from cloud-config-server

--- movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/movierating/MovieRatingServiceApplication.java
+++ movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/movierating/MovieRatingServiceApplication.java
@@ -2,8 +2,10 @@ package com.ssp.apps.microservices.movierating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieRatingServiceApplication {
 
     public static void main(String[] args) {
--- movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/movierating/controller/MovieRatingController.java
+++ movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/movierating/controller/MovieRatingController.java
@@ -1,5 +1,6 @@
 package com.ssp.apps.microservices.movierating.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/movie-rating")
 public class MovieRatingController {
 
+    @Value("${app_name}")
+    private String appName;
+
     @GetMapping
     public String checkStatus() {
-        return "Working...!!!";
+        return "Working...!!!, APP_NAME : " + appName;
     }
 }
