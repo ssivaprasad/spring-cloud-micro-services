29eacbd8ea5c3d1213679c7e562f7f470beff08b
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-05 11:33:14 +0530

    cloud-config-server

---
+++ cloud-config-server/src/test/java/com/ssp/apps/cloud/configserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssp.apps.cloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
