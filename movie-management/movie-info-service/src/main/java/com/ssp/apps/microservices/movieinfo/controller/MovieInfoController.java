91be888633d52603e62eea0c1ac3b0b6da8380d6
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-05 22:49:07 +0530

    Add Feign client dependency write Feign Client for movie-rating-service and call movie-rating-service from movie-info-service

--- movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/controller/MovieInfoController.java
+++ movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/controller/MovieInfoController.java
@@ -1,9 +1,11 @@
 package com.ssp.apps.microservices.movieinfo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.ssp.apps.microservices.movieinfo.feignclient.MovieRatingClient;
 
 @RestController
 @RequestMapping("/movie-info")
@@ -12,8 +14,13 @@ public class MovieInfoController {
     @Value("${app_name}")
     private String appName;
 
+    @Autowired
+    private MovieRatingClient movieRatingClient;
+
     @GetMapping
     public String checkStatus() {
+        String response = movieRatingClient.checkStatus();
+        System.out.println("==== >> Response: " + response);
         return "Working...!!!, APP_NAME : " + appName;
     }
 }
