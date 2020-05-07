13fdaf0fc2f2ee208dbad4538b0a9b03bd560590
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-07 11:53:12 +0530

    Add Spring-data-jpa and h2 dependencies and EnableJpaRepositores

--- user-management/src/main/java/com/ssp/apps/cloud/user/UserManagementApplication.java
+++ user-management/src/main/java/com/ssp/apps/cloud/user/UserManagementApplication.java
@@ -2,8 +2,10 @@ package com.ssp.apps.cloud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class UserManagementApplication {
 
     public static void main(String[] args) {
