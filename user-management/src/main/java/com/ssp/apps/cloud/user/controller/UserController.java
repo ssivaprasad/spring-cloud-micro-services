7969fbad33b0d3d26b682b4b5475a37fe8d5d822
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-07 10:49:16 +0530

    Add Basic UserController

---
+++ user-management/src/main/java/com/ssp/apps/cloud/user/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.ssp.apps.cloud.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String checkStatus() {
+        return "Working... !!!";
+    }
+
+}
