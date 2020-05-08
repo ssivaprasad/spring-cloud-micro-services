c43fb2567fb67f7bf9f7d97662953f2d81cfea55
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-08 19:20:06 +0530

    Enable Resource Server

--- movie-management/movie-catalog-service/src/main/java/com/ssp/apps/movie/catalog/MovieCatalogServiceApplication.java
+++ movie-management/movie-catalog-service/src/main/java/com/ssp/apps/movie/catalog/MovieCatalogServiceApplication.java
@@ -2,12 +2,14 @@ package com.ssp.apps.movie.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class MovieCatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+    }
 
 }
