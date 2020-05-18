78d36a3bfc53b624a969af43675d084780bde2b3
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-18 13:05:26 +0530

    Implement /checkstatus endpoint for movie-info-service and movie-rating-service

--- movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/controller/MovieInfoController.java
+++ movie-management/movie-info-service/src/main/java/com/ssp/apps/microservices/movieinfo/controller/MovieInfoController.java
@@ -2,10 +2,10 @@ package com.ssp.apps.microservices.movieinfo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.ssp.apps.microservices.movieinfo.feignclient.MovieRatingClient;
 
 @RestController
 @RequestMapping("/movie-info")
@@ -15,12 +15,12 @@ public class MovieInfoController {
     private String appName;
 
     @Autowired
-    private MovieRatingClient movieRatingClient;
+    private Environment environment;
 
-    @GetMapping
+    @GetMapping("checkstatus")
     public String checkStatus() {
-        String response = movieRatingClient.checkStatus();
-        System.out.println("==== >> Response: " + response);
-        return "Working...!!!, APP_NAME : " + appName;
+        return "Working...!!!,  APP_NAME : " + appName + ", Up and Running on Port: "
+                + environment.getProperty("local.server.port");
     }
+
 }
--- movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/movierating/controller/MovieRatingController.java
+++ movie-management/movie-rating-service/src/main/java/com/ssp/apps/microservices/movierating/controller/MovieRatingController.java
@@ -1,6 +1,8 @@
 package com.ssp.apps.microservices.movierating.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,12 @@ public class MovieRatingController {
     @Value("${spring.application.name}")
     private String appName;
 
-    @GetMapping
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("checkstatus")
     public String checkStatus() {
-        return "Working...!!!, APP_NAME : " + appName;
+        return "Working...!!!,  APP_NAME : " + appName + ", Up and Running on Port: "
+                + environment.getProperty("local.server.port");
     }
 }
