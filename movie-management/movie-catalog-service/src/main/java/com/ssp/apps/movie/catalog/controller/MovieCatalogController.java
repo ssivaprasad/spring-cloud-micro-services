ce9f3f74f334c975ae2a565339ef58079330d6e1
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-18 13:21:36 +0530

    change movie-catalog-service endpoint

--- movie-management/movie-catalog-service/src/main/java/com/ssp/apps/movie/catalog/controller/MovieCatalogController.java
+++ movie-management/movie-catalog-service/src/main/java/com/ssp/apps/movie/catalog/controller/MovieCatalogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/movie-catalog")
 public class MovieCatalogController {
 
     @Value("${spring.application.name}")
