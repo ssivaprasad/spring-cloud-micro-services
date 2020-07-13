1e43bf9ecc25ba918c1a3876515a878e09fcb218
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-07-13 23:11:04 +0530

    book-service application

--- library-management/book-service/src/main/java/com/ssp/apps/library/manament/books/BookServiceApplication.java
+++ library-management/book-service/src/main/java/com/ssp/apps/library/manament/books/BookServiceApplication.java
@@ -2,7 +2,9 @@ package com.ssp.apps.library.manament.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BookServiceApplication {
 
