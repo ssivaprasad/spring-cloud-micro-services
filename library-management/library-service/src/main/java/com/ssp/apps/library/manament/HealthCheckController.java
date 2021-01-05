52c439d4edb87e858f7d85bd8b943a1c73350315
sivaprasads <somarouthu.sivaprasads@gmail.com>
2021-01-05 14:09:23 +0530

    library-service

--- library-management/library-service/src/main/java/com/ssp/apps/library/manament/HealthCheckController.java
+++ library-management/library-service/src/main/java/com/ssp/apps/library/manament/HealthCheckController.java
@@ -1,16 +1,18 @@
 package com.ssp.apps.library.manament;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
-public class LibraryStatusController {
+public class HealthCheckController {
 
     @Value("${app.status.check.message}")
     private String appStatusCheckMessage;
 
-    @GetMapping("/checkstatus")
+    @GetMapping("/healthcheck")
     public String checkStatus() {
         return appStatusCheckMessage;
     }
