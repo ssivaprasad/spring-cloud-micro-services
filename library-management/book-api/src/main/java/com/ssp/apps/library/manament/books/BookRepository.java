2eadabf477a2ed6c439d4db908398786e460abab
sivaprasads <somarouthu.sivaprasads@gmail.com>
2022-12-12 13:30:54 +0530

    cleanup

--- library-management/book-api/src/main/java/com/ssp/apps/library/manament/books/BookRepository.java
+++ library-management/book-api/src/main/java/com/ssp/apps/library/manament/books/BookRepository.java
@@ -1,6 +1,7 @@
 package com.ssp.apps.library.manament.books;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
 public interface BookRepository extends CrudRepository<Book, Long> {
--- library-management/book-api/src/main/java/com/ssp/apps/library/manament/books/BookServiceApplication.java
+++ library-management/book-api/src/main/java/com/ssp/apps/library/manament/books/BookServiceApplication.java
@@ -1,18 +1,16 @@
 package com.ssp.apps.library.manament.books;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookServiceApplication{
-@Autowired
-    private BookRepository repository;
+public class BookServiceApplication {
+	@Autowired
+	private BookRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
-  
 
 }
