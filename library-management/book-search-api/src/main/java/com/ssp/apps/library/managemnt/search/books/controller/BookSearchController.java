511b9d776b11c7934128779a0287efe42a4cd1aa
sivaprasads <somarouthu.sivaprasads@gmail.com>
2022-12-14 13:26:17 +0530

    add servlet context paths

--- library-management/book-search-api/src/main/java/com/ssp/apps/library/managemnt/search/books/controller/BookSearchController.java
+++ library-management/book-search-api/src/main/java/com/ssp/apps/library/managemnt/search/books/controller/BookSearchController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/book-search")
+@RequestMapping("/books")
 public class BookSearchController {
     @Autowired
     private BookRepository bookRepository;
