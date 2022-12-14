600158dad9e30c9272db8ebb6f18b680452a2d3c
sivaprasads <somarouthu.sivaprasads@gmail.com>
2022-12-14 05:52:14 +0530

    cloud api gateway

---
+++ cloud-api-gateway/src/main/java/com/ssp/apps/cloud/apigateway/CloudApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ssp.apps.cloud.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiGatewayApplication.class, args);
+	}
+
+}
