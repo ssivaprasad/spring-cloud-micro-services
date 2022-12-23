b3e450984727112dc6472e19a5541792a45bbc8b
sivaprasads <somarouthu.sivaprasads@gmail.com>
2022-12-23 18:35:41 +0530

    cleanup

--- cloud-api-gateway/src/main/java/com/ssp/apps/cloud/apigateway/CloudApiGatewayApplication.java
+++ cloud-api-gateway/src/main/java/com/ssp/apps/cloud/apigateway/CloudApiGatewayApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class CloudApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApiGatewayApplication.class, args);
+    }
 
 }
